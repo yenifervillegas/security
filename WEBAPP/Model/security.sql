3db620db66406834966756e05aee51b8556c6462
yenifervillegas <yataborda75@misena.edu.co>
2016-10-24 13:47:37 -0500

    sql

--- WEBAPP/Model/security.sql
+++ WEBAPP/Model/security.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2016 a las 16:40:34
+-- Tiempo de generación: 24-10-2016 a las 20:47:04
 -- Versión del servidor: 10.1.10-MariaDB
 -- Versión de PHP: 5.6.19
 
@@ -35,6 +35,13 @@ CREATE TABLE `accesorios` (
   `acce_cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `accesorios`
+--
+
+INSERT INTO `accesorios` (`acce_cod`, `regi_cod`, `acce_nom`, `acce_cantidad`) VALUES
+(1, 1, 'mause', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +57,16 @@ CREATE TABLE `entrada_salida` (
   `entra_horasal` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `entrada_salida`
+--
+
+INSERT INTO `entrada_salida` (`entra_cod`, `regi_cod`, `entra_fechaentra`, `entra_fechasal`, `entra_horaentra`, `entra_horasal`) VALUES
+(1, 1, '16-10-24', '', '13:39,PM', ''),
+(2, 1, '16-10-24', '', '13:39,PM', ''),
+(3, 1, '16-10-24', '', '13:40,PM', ''),
+(4, 1, '16-10-24', '', '13:42,PM', '');
+
 -- --------------------------------------------------------
 
 --
@@ -58,15 +75,27 @@ CREATE TABLE `entrada_salida` (
 
 CREATE TABLE `registro_producto` (
   `regi_cod` int(11) NOT NULL,
-  `usu_cod` int(11) NOT NULL,
+  `usu_cod` varchar(20) NOT NULL,
   `produ_cod` int(11) NOT NULL,
   `regi_serial` varchar(80) NOT NULL,
+  `regi_marca` varchar(50) NOT NULL,
   `regi_color` varchar(80) NOT NULL,
   `regi_fecha` date NOT NULL,
   `regi_desc` varchar(80) NOT NULL,
-  `regi_autoalerta` varchar(80) NOT NULL,
-  `regi_cantidad` int(11) NOT NULL
+  `regi_autoalerta` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `registro_producto`
+--
+
+INSERT INTO `registro_producto` (`regi_cod`, `usu_cod`, `produ_cod`, `regi_serial`, `regi_marca`, `regi_color`, `regi_fecha`, `regi_desc`, `regi_autoalerta`) VALUES
+(1, '98111364257', 1, '212121', 'adsd', 'adsd', '0000-00-00', 'adsd', 'adsd'),
+(2, '98111364257', 1, '212121', 'adsd', 'adsd', '0000-00-00', 'adsd', 'adsd'),
+(3, '98111364257', 1, '123456', 'ssss', '#000000', '2016-10-24', 'dfgdfg', 'dgdfgd'),
+(4, '98111364257', 1, '123456', 'fhgv', '#000000', '2016-10-24', 'ghfgh', 'fghgfhf'),
+(5, '2333333', 1, '222', 'fhgv', '#000000', '2016-10-24', '', ''),
+(6, '123456789', 1, '123456', 'uiyuiyu', '#000000', '2016-10-24', '', '');
 
 -- --------------------------------------------------------
 
@@ -94,9 +123,15 @@ INSERT INTO `rol` (`rol_cod`, `rol_nom`) VALUES
 
 CREATE TABLE `tipo_producto` (
   `produ_cod` int(11) NOT NULL,
-  `produ_nom` varchar(80) NOT NULL,
-  `produ_marca` varchar(50) NOT NULL
+  `produ_nom` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipo_producto`
+--
+
+INSERT INTO `tipo_producto` (`produ_cod`, `produ_nom`) VALUES
+(1, 'portatil');
 
 -- --------------------------------------------------------
 
@@ -125,7 +160,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`usu_cod`, `rol_cod`, `usu_nom`, `usu_ape`, `usu_tipodocu`, `usu_docu`, `usu_email`, `usu_tel`, `usu_direc`, `usu_centro`, `usu_estado`, `usu_cargo`, `usu_pass`) VALUES
-(1, 1, 'Andrea', 'Taborda', 'Cedula', '98111364257', 'andre@homail.com', 3232, 'direccion', 'centro', 'Activo', 'cargo', '$2y$10$RLdLyq4byBD/sXhmzhRoVuPqVc9kcTDUn0quO2qd.fF3r/PcFrPwu');
+(1, 1, 'andrea', 'taborda', 'Tarjeta de Identidad', '98111364257', 'yenitabordav@hotmail.com', 456, 'dfd', 'dsds', 'Activo', 'ff', '$2y$10$VlB0rYkvP28twW8se4TfgeSb4UAmzpjWzpCZZMraMfnQh21t/q3.y'),
+(2, 1, 'cristian', 'villegas', 'Cedula', '123456789', 'asd@fg.com', 456, 'efgdfg', 'calatrava', 'Activo', 'vigilante', '$2y$10$ZX/U6p2NhXLWOihL.SinfOApOQzq8Z.QBC6FO2F.sFdLo3sbtZ6hm');
 
 --
 -- Índices para tablas volcadas
@@ -136,8 +172,7 @@ INSERT INTO `usuario` (`usu_cod`, `rol_cod`, `usu_nom`, `usu_ape`, `usu_tipodocu
 --
 ALTER TABLE `accesorios`
   ADD PRIMARY KEY (`acce_cod`),
-  ADD KEY `regi_cod` (`regi_cod`),
-  ADD KEY `regi_cod_2` (`regi_cod`);
+  ADD KEY `regi_cod` (`regi_cod`);
 
 --
 -- Indices de la tabla `entrada_salida`
@@ -181,17 +216,17 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `accesorios`
 --
 ALTER TABLE `accesorios`
-  MODIFY `acce_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `acce_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `entrada_salida`
 --
 ALTER TABLE `entrada_salida`
-  MODIFY `entra_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `entra_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `registro_producto`
 --
 ALTER TABLE `registro_producto`
-  MODIFY `regi_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `regi_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
@@ -201,12 +236,12 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `tipo_producto`
 --
 ALTER TABLE `tipo_producto`
-  MODIFY `produ_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `produ_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usu_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `usu_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Restricciones para tablas volcadas
 --
